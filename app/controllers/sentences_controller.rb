e8e461fd0ce4d1f371992c85cf4bdeb3da46ec99
Beccaneer <thomson.rebecca@gmail.com>
2017-03-05 16:49:00 -0500

    dropdowns for sentences working (needs testing)

--- app/controllers/sentences_controller.rb
+++ app/controllers/sentences_controller.rb
@@ -1,2 +1,29 @@
 class SentencesController < ApplicationController
+	protect_from_forgery
+  	before_filter :set_var
+
+  
+	def show
+		@sentence = Sentence.find(params[:id])
+		@structure = @sentence.structure
+	end
+
+	def get_dropdown(type_array)
+		puts "********************************************************************"
+		puts "in dropdown method"
+		@total_array = []
+		type_array.each do |type|
+			puts "*****************************************************************"
+			puts type
+			@words = Word.where(classifier: type).pluck(:id)
+			@total_array.push(@words)
+		end		
+		@total_array
+	end 
+	helper_method :get_dropdown
+
+	private
+  	def set_var
+    	@total_array = []
+  	end
 end
--- app/models/sentence.rb
+++ app/models/sentence.rb
@@ -1,2 +1,4 @@
 class Sentence < ApplicationRecord
+	serialize :structure, Array
+	serialize :meaning, Array
 end
